036dc22675d91b5edc5b0251261c5e4e0a096226
Philip Piper <piper.m.philip@gmail.com>
2022-03-20 16:34:58 -0700

    Added a simple javascript sidebar, and some simple CSS.

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 # Create the application host
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80)
+    app.run(host='127.0.0.1', port=8080, debug=True)
