810e456fc0dc277bc26104a24be116255df93263
Philip Piper <piper.m.philip@gmail.com>
2022-03-21 11:11:06 -0700

    Sidebar portrait and name added.
    This should be enough sidebar work for now. Need to stop fiddling with
    this and start focusing on the actual website content!

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -21,7 +21,8 @@ function closeNav() {
     sidebarState = 0;
     $("#main-sidebar").css("width", "0");
     $("#main").css("marginLeft", "0");
-    $("#main-sidebar").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+    $("#main-sidebar")
+        .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
         function(e) {
             $("#main").css("white-space", "normal");
             $(this).off(e);
@@ -29,16 +30,23 @@ function closeNav() {
 }
 
 function sidebarResize() {
+    // Skip if the sidebar is off
     if (!sidebarState)
         return;
 
+    // Resize the window
     if ($(window).width() < 500) {
         width = $(window).width();
+        $("#headshot").css("display", "none");
     } else {
         width = 250;
+        $("#headshot").css("display", "block");
     }
-    width = width.toString() + "px";
+    widthPx = width.toString() + "px";
     $("#main-sidebar").css("width", width);
     $("#main").css("margin-left", width);
     $("#main").css("white-space", "nowrap");
+
+    // Resize the sidebar font based on window width
+    $("#main-sidebar").find("a").css("font-size", width*0.1);
 }
