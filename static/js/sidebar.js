14426a4d4e1fbc5856d774475049d0c380e30f53
Philip Piper <piper.m.philip@gmail.com>
2022-03-21 17:01:57 -0700

    Fixed up sidebar, finished contact, added fontawesome, and swapped to bootstrap grids.

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,52 +1,48 @@
 $(document).ready(function() {
-    sidebarState = $(window).width() >= 1000;
+    window.sidebarWidth = 280;
+    $("#headshot-picture").css("width",
+        (window.sidebarWidth-100).toString()+"px");
+    window.sidebarState = $(window).width() >= 1000;
     sidebarResize();
 });
 
 $(window).on("resize", function() {
     clearTimeout(window.sidebarTimer);
-    window.sidebarTimer = setTimeout(sidebarResize, 500);
+    window.sidebarTimer = setTimeout(sidebarResize, 200);
 });
 
 /* Set the width of the sidebar to 250px and the left margin of the page
  * content to 250px */
-function openNav() {
-    sidebarState = 1;
+function sidebarOpen() {
+    window.sidebarState = 1;
     sidebarResize();
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page
  * content to 0 */
-function closeNav() {
-    sidebarState = 0;
+function sidebarClose() {
+    window.sidebarState = 0;
     $("#main-sidebar").css("width", "0");
     $("#main").css("marginLeft", "0");
-    $("#main-sidebar")
-        .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
-        function(e) {
-            $("#main").css("white-space", "normal");
-            $(this).off(e);
-        });
 }
 
 function sidebarResize() {
     // Skip if the sidebar is off
-    if (!sidebarState)
+    if (!window.sidebarState)
         return;
 
     // Resize the window
-    if ($(window).width() < 500) {
+    if ($(window).width() < 768) {
         width = $(window).width();
         $("#headshot").css("display", "none");
     } else {
-        width = 250;
+        width = window.sidebarWidth;
         $("#headshot").css("display", "block");
     }
     widthPx = width.toString() + "px";
     $("#main-sidebar").css("width", width);
     $("#main").css("margin-left", width);
-    $("#main").css("white-space", "nowrap");
 
     // Resize the sidebar font based on window width
-    $("#main-sidebar").find("a").css("font-size", width*0.1);
+    $("#main-sidebar").find("a").css("font-size", width*0.09);
 }
